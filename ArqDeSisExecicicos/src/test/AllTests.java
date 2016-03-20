79a972868acb5ffc7bc72cd5125812972dbaf5e8
guilherme <gui.souza31@gmail.com>
2016-03-20 20:47:44 -0300

    Classes de teste adicionadas

--- ArqDeSisExecicicos/src/test/AllTests.java
+++ ArqDeSisExecicicos/src/test/AllTests.java
@@ -5,8 +5,9 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ ConnectionFactoryTest.class, ExtratoDAOTest.class,
-		ExtratoTest.class, ExtratoTOTest.class })
+@SuiteClasses({ ConnectionFactoryTest.class, 
+				ExtratoDAOTest.class, ExtratoTest.class, ExtratoTOTest.class, 
+				SaqueDAOTest.class, SaqueTest.class, SaqueTOTest.class})
 public class AllTests {
 
 }
